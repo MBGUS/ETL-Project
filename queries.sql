e765e483bb75800ae5b7b2faebb3325548f60f09
Marcelo Flores <79871752+marceloflores1@users.noreply.github.com>
2021-06-12 16:04:07 -0500

    Modifying queries.sql

--- queries.sql
+++ queries.sql
@@ -1,69 +1,46 @@
-DROP TABLE IF EXISTS "INEGI";
-DROP TABLE IF EXISTS "SNIIV";
-DROP TABLE IF EXISTS "ENTIDAD";
-DROP TABLE IF EXISTS "MUNICIPIO";
-DROP TABLE IF EXISTS "Genero";
-DROP TABLE IF EXISTS "Rango_Edad";
-
-CREATE TABLE "INEGI" (
-    "Clave_Municipio" Int   NOT NULL,
-    "Pob_Total" Int   NOT NULL,
-    "Pob_Fem" Int   NOT NULL,
-    "Pob_Mas" Int   NOT NULL
-);
-
-CREATE TABLE "SNIIV" (
-    "Clave_Municipio" Int   NOT NULL,
-    "ID_Genero" Int   NOT NULL,
-    "ID_Rango_Edad" Int   NOT NULL,
-    "Prestamo" Int   NOT NULL
-);
+DROP TABLE IF EXISTS "ENTIDAD" CASCADE;
+DROP TABLE IF EXISTS "MUNICIPIO" CASCADE;
+DROP TABLE IF EXISTS "INEGI" CASCADE;
+DROP TABLE IF EXISTS "Genero" CASCADE;
+DROP TABLE IF EXISTS "Rango_Edad" CASCADE;
+DROP TABLE IF EXISTS "SNIIV" CASCADE;
 
 CREATE TABLE "ENTIDAD" (
-    "Clave_Entidad" Int   NOT NULL,
-    "Entidad_Fed" Varchar   NOT NULL,
-    CONSTRAINT "pk_ENTIDAD" PRIMARY KEY (
-        "Clave_Entidad"
-     )
+    "Clave_Entidad" INT PRIMARY KEY,
+    "Entidad_Fed" Varchar
 );
 
 CREATE TABLE "MUNICIPIO" (
-    "Clave_Municipio" Int   NOT NULL,
-    "Munipio" Varchar   NOT NULL,
-    "Clave_Entidad" Int   NOT NULL,
-    CONSTRAINT "pk_MUNICIPIO" PRIMARY KEY (
-        "Clave_Municipio"
-     )
+    "Clave_Municipio" Varchar PRIMARY KEY,
+    "Municipio" Varchar,
+    "Clave_Entidad" Int,
+	FOREIGN KEY ("Clave_Entidad") REFERENCES "ENTIDAD"("Clave_Entidad")
+);
+
+CREATE TABLE "INEGI" (
+    "Clave_Municipio" Varchar,
+    "Pob_Total" Int,
+    "Pob_Fem" Int,
+    "Pob_Mas" Int,
+	FOREIGN KEY ("Clave_Municipio") REFERENCES "MUNICIPIO"("Clave_Municipio")
 );
 
 CREATE TABLE "Genero" (
-    "ID" Int   NOT NULL,
-    "Genero" Varchar   NOT NULL,
-    CONSTRAINT "pk_Genero" PRIMARY KEY (
-        "ID"
-     )
+    "ID" Varchar PRIMARY KEY,
+    "Genero" Varchar
 );
 
 CREATE TABLE "Rango_Edad" (
-    "ID" Int   NOT NULL,
-    "Rango" Varchar   NOT NULL,
-    CONSTRAINT "pk_Rango_Edad" PRIMARY KEY (
-        "ID"
-     )
+    "ID" Varchar PRIMARY KEY,
+    "Rango" Varchar
 );
 
-ALTER TABLE "INEGI" ADD CONSTRAINT "fk_INEGI_Clave_Municipio" FOREIGN KEY("Clave_Municipio")
-REFERENCES "MUNICIPIO" ("Clave_Municipio");
-
-ALTER TABLE "SNIIV" ADD CONSTRAINT "fk_SNIIV_Clave_Municipio" FOREIGN KEY("Clave_Municipio")
-REFERENCES "MUNICIPIO" ("Clave_Municipio");
-
-ALTER TABLE "SNIIV" ADD CONSTRAINT "fk_SNIIV_ID_Genero" FOREIGN KEY("ID_Genero")
-REFERENCES "Genero" ("ID");
-
-ALTER TABLE "SNIIV" ADD CONSTRAINT "fk_SNIIV_ID_Rango_Edad" FOREIGN KEY("ID_Rango_Edad")
-REFERENCES "Rango_Edad" ("ID");
-
-ALTER TABLE "MUNICIPIO" ADD CONSTRAINT "fk_MUNICIPIO_Clave_Entidad" FOREIGN KEY("Clave_Entidad")
-REFERENCES "ENTIDAD" ("Clave_Entidad");
-
+CREATE TABLE "SNIIV" (
+    "Clave_Municipio" Varchar,
+    "ID_Genero" Varchar,
+    "ID_Rango_Edad" Varchar,
+    "Prestamo" Varchar, 
+	FOREIGN KEY ("Clave_Municipio") REFERENCES "MUNICIPIO"("Clave_Municipio"), 
+	FOREIGN KEY ("ID_Genero") REFERENCES "Genero"("ID"),
+	FOREIGN KEY ("ID_Rango_Edad") REFERENCES "Rango_Edad"("ID")
+);
